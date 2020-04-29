38cb43ffea56382f38b09b4912d293c373f26e49
Umang Chadha <umangchadha@Umangs-MBP.home>
2020-04-28 22:42:47 -0400

    graph

--- src/app/india/india.component.ts
+++ src/app/india/india.component.ts
@@ -14,143 +14,33 @@ export class IndiaComponent implements OnInit {
   masterData: any = {};
   india = {};
   statewiseData: any;
-  view: any[] = [600, 400];
+  view: any[] = [600, 300];
   timeSeries: any;
 
 
-  // options for the chart
-  showXAxis = true;
-  showYAxis = true;
-  gradient = false;
-  showLegend = true;
-  showXAxisLabel = true;
-  xAxisLabel = 'Country';
+  // options
+  legend = false;
+  showLabels = false;
+  showGridLines = false;
+  animations = true;
+  xAxis = true;
+  yAxis = true;
   showYAxisLabel = true;
-  yAxisLabel = 'Sales';
-  timeline = true;
+  showXAxisLabel = true;
+  xAxisLabel = 'Dates';
+  yAxisLabel = 'Cases';
+
+
 
   colorScheme = {
-    domain: ['#9370DB', '#87CEFA', '#FA8072', '#FF7F50', '#90EE90', '#9370DB']
+    domain: ['#CFC0BB', '#5AA454', '#E44D25']
   };
-  public single = [
-    {
-      "name": "China",
-      "value": 2243772
-    },
-    {
-      "name": "USA",
-      "value": 1126000
-    },
-    {
-      "name": "Norway",
-      "value": 296215
-    },
-    {
-      "name": "Japan",
-      "value": 257363
-    },
-    {
-      "name": "Germany",
-      "value": 196750
-    },
-    {
-      "name": "France",
-      "value": 204617
-    }
-  ];
-
-  public multi = [
-    {
-      "name": "China",
-      "series": [
-        {
-          "name": "2018",
-          "value": 2243772
-        },
-        {
-          "name": "2017",
-          "value": 1227770
-        }
-      ]
-    },
-
-    {
-      "name": "USA",
-      "series": [
-        {
-          "name": "2018",
-          "value": 1126000
-        },
-        {
-          "name": "2017",
-          "value": 764666
-        }
-      ]
-    },
-
-    {
-      "name": "Norway",
-      "series": [
-        {
-          "name": "2018",
-          "value": 296215
-        },
-        {
-          "name": "2017",
-          "value": 209122
-        }
-      ]
-    },
-
-    {
-      "name": "Japan",
-      "series": [
-        {
-          "name": "2018",
-          "value": 257363
-        },
-        {
-          "name": "2017",
-          "value": 205350
-        }
-      ]
-    },
-
-    {
-      "name": "Germany",
-      "series": [
-        {
-          "name": "2018",
-          "value": 196750
-        },
-        {
-          "name": "2017",
-          "value": 129246
-        }
-      ]
-    },
-
-    {
-      "name": "France",
-      "series": [
-        {
-          "name": "2018",
-          "value": 204617
-        },
-        {
-          "name": "2017",
-          "value": 149797
-        }
-      ]
-    }
-  ];
-
 
   constructor(private httpClient: HttpClient, public message: MessageService) { }
 
   ngOnInit() {
     this.getData();
-
+    this.getStateData();
     setInterval(() => {
       this.getData();
     }, 10000); // 10 sec interval
@@ -162,7 +52,6 @@ export class IndiaComponent implements OnInit {
       .subscribe((a: any) => {
         this.statewiseData = a.statewise;
         this.india = _.filter(this.statewiseData, (b: any) => b.state === 'Total');
-        this.getStateData();
       });
 
     this.message.spinner = false;
@@ -177,57 +66,47 @@ export class IndiaComponent implements OnInit {
 
   createGraphData(stateCode) {
     if (this.timeSeries) {
-      let seriesArr = [];
-
-      this.timeSeries.states_daily.map(a => {
-        if (a.status === 'Confirmed') {
-          const x = {
-            date: a.date,
-            confirmed: a.stateCode,
-          };
-          seriesArr.push(x);
-        }
+      const recoverArr = [];
+      const confirmedArr = [];
+      const deceasedArr = [];
+      this.timeSeries.states_daily.filter(a => {
         if (a.status === 'Recovered') {
           const x = {
-            date: a.date,
-            recovered: a.stateCode
+            value: a[stateCode],
+            name: a.date
           };
-          seriesArr.push(x);
+          recoverArr.push(x);
+        }
+        if (a.status === 'Confirmed') {
+          const x = {
+            value: a[stateCode],
+            name: a.date
+          };
+          confirmedArr.push(x);
         }
         if (a.status === 'Deceased') {
           const x = {
-            date: a.date,
-            deceased: a.stateCode
+            value: a[stateCode],
+            name: a.date
           };
-          seriesArr.push(x);
+          deceasedArr.push(x);
         }
       });
-
-      const uniqDates = _.uniq(seriesArr.map(a => a.date));
-
-      const sA = uniqDates.map(a => {
-        const y: any = {};
-        seriesArr.filter(b => {
-          y.date = b.date;
-          if (b.date === a && b.confirmed) {
-            y.confirmed = b.confirmed;
-          }
-          if (b.date === a && b.deceased) {
-            y.deceased = b.deceased;
-          }
-          if (b.date === a && b.recovered) {
-            y.recovered = b.recovered;
-          }
-        });
-
-        return y;
-      });
-      seriesArr = sA;
       const obj = [{
-        name: stateCode,
-        series: seriesArr
-      }];
-      console.log(obj);
+        name: 'Confirmed',
+        series: confirmedArr
+      },
+      {
+        name: 'Recovered',
+        series: recoverArr
+      },
+      {
+        name: 'Deceased',
+        series: deceasedArr
+      }
+      ];
+
+      return obj;
     }
   }
 }
