f2c88cb2989e5f56b317d3e33ef34ecce4a960a6
Umang Chadha <umang.chadha@bnymellon.com>
2020-04-29 13:19:16 -0400

    graph modification

--- src/app/india/india.component.ts
+++ src/app/india/india.component.ts
@@ -30,10 +30,12 @@ export class IndiaComponent implements OnInit {
     }, 10000); // 10 sec interval
   }
 
-  openDialog(statecode, state): void {
+  openDialog(obj, state): void {
+
     const dialogRef = this.dialog.open(DialogOverviewDialogComponent, {
       width: '100%',
-      data: { allData: this.timeSeries, stateCode: statecode.toLowerCase(), stateName: state }
+      height: '50%',
+      data: { allData: obj, stateName: state.state }
     });
 
     dialogRef.afterClosed().subscribe(result => {
@@ -53,65 +55,37 @@ export class IndiaComponent implements OnInit {
   }
 
   getStateData() {
+
     this.httpClient.get('https://api.covid19india.org/states_daily.json')
       .subscribe((a: any) => {
         this.timeSeries = a;
       });
   }
 
-
-}
-
-@Component({
-  selector: 'app-overview-example-dialog',
-  templateUrl: 'example-dialog.html',
-})
-export class DialogOverviewDialogComponent {
-
-  // options
-
-  showLabels = false;
-  showGridLines = false;
-  xAxis = true;
-  yAxis = true;
-  showYAxisLabel = true;
-  showXAxisLabel = true;
-  xAxisLabel = 'Dates';
-  yAxisLabel = 'Cases';
-
-
-  colorScheme = {
-    domain: ['#CFC0BB', '#5AA454', '#E44D25']
-  };
-  constructor(
-    public dialogRef: MatDialogRef<DialogOverviewDialogComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: any) {
-
-  }
-
-  createGraphData() {
-    if (this.data.allData) {
+  createGraphData(state) {
+    this.message.spinner = true;
+    if (this.timeSeries) {
       const recoverArr = [];
       const confirmedArr = [];
       const deceasedArr = [];
-      this.data.allData.states_daily.filter(a => {
+      this.timeSeries.states_daily.filter(a => {
         if (a.status === 'Recovered') {
           const x = {
-            value: a[this.data.stateCode],
+            value: a[state.statecode.toLowerCase()],
             name: a.date
           };
           recoverArr.push(x);
         }
         if (a.status === 'Confirmed') {
           const x = {
-            value: a[this.data.stateCode],
+            value: a[state.statecode.toLowerCase()],
             name: a.date
           };
           confirmedArr.push(x);
         }
         if (a.status === 'Deceased') {
           const x = {
-            value: a[this.data.stateCode],
+            value: a[state.statecode.toLowerCase()],
             name: a.date
           };
           deceasedArr.push(x);
@@ -130,10 +104,41 @@ export class DialogOverviewDialogComponent {
         series: deceasedArr
       }
       ];
-
-      return obj;
+      this.openDialog(obj, state);
+      this.message.spinner = false;
     }
   }
+
+
+}
+
+@Component({
+  selector: 'app-overview-example-dialog',
+  templateUrl: 'example-dialog.html',
+})
+export class DialogOverviewDialogComponent {
+
+  // options
+  view = ['100%', '100%']
+  showLabels = false;
+  showGridLines = false;
+  xAxis = true;
+  yAxis = true;
+  showYAxisLabel = true;
+  showXAxisLabel = true;
+  xAxisLabel = 'Dates';
+  yAxisLabel = 'Cases';
+  results = [];
+
+  colorScheme = {
+    domain: ['#CFC0BB', '#5AA454', '#E44D25']
+  };
+  constructor(
+    public dialogRef: MatDialogRef<DialogOverviewDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any) {
+  }
+
+
   onNoClick(): void {
     this.dialogRef.close();
   }
