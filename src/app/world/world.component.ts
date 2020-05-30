bdb732b831b437abdf8b53c4346f1913baffb0c0
Ankit Anand <ankitanand@Ankits-MacBook-Pro.local>
2020-05-30 19:13:11 +0530

    active added

--- src/app/world/world.component.ts
+++ src/app/world/world.component.ts
@@ -44,6 +44,8 @@ export class WorldComponent implements OnChanges, OnDestroy {
       this.masterData = a;
       this.masterData.countries_stat = _.orderBy(this.masterData.countries_stat,
         [obj => parseFloat(obj.cases.replace(/,/g, ''))], ['desc']);
+      this.masterData.countries_stat.map( (a) => a.percentage= ((parseInt(a.total_recovered.replace(/\,/g,''))/parseInt(a.cases.replace(/\,/g,'')))*100).toFixed(2));
+      this.masterData.countries_stat.map( (a) => a.death_percentage= ((parseInt(a.deaths.replace(/\,/g,''))/parseInt     (a.cases.replace(/\,/g,'')))*100).toFixed(2));
       this.searchedData = _.cloneDeep(this.masterData.countries_stat);
       this.updateTimestamp = this.masterData.statistic_taken_at;
       this.messageService.spinner = false;
