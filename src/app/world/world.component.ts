0304fdecbc1f17413f600d6e4e8730bbda501a44
Ankit Anand <ankitanand@Ankits-MacBook-Pro.local>
2020-06-08 15:20:17 +0530

    sorting added

--- src/app/world/world.component.ts
+++ src/app/world/world.component.ts
@@ -14,6 +14,14 @@ import * as _ from 'lodash';
 export class WorldComponent implements OnChanges, OnDestroy {
   title = 'covid';
   masterData: any = {};
+  selectOptions: any = [
+    {value: 'nofilter', viewValue: 'No Filter'},
+    {value: 'confirmed', viewValue: 'Confirmed'},
+    {value: 'deaths', viewValue: 'Deaths'},
+    {value: 'recovered', viewValue: 'Recovered'},
+    {value: 'active', viewValue: 'Active'},
+    {value: 'alphabetical', viewValue: 'Alphabetical'},
+  ];
   show: any = false;
   dataInterval: any;
   filterName: any;
@@ -54,6 +62,57 @@ export class WorldComponent implements OnChanges, OnDestroy {
     });
 
   }
+
+
+  sortData(event){
+    if ( event == "deaths" ){
+      this.masterData.countries_stat.sort(function (a, b) {
+      return parseInt((b.deaths).replace(/\,/g,'')) - parseInt((a.deaths).replace(/\,/g,''));
+    })
+  }
+  else if(event == "confirmed"){
+        this.masterData.countries_stat.sort(function (a, b) {
+        return parseInt((b.cases).replace(/\,/g,'')) - parseInt((a.cases).replace(/\,/g,''));
+    })
+  }
+  else if(event == "recovered"){
+    let temp = [];
+    let temp1 = [];
+    temp = this.masterData.countries_stat.filter(a => a.total_recovered != "N/A");
+    temp1 = this.masterData.countries_stat.filter(a => a.total_recovered == "N/A");
+    this.masterData.countries_stat = [...temp,...temp1];
+    this.masterData.countries_stat.sort(function (a, b) {
+          return parseInt((b.total_recovered).replace(/\,/g,'')) - parseInt((a.total_recovered).replace(/\,/g,''));
+    })
+  }
+  else if(event == "active"){
+    let temp = [];
+    let temp1 = [];
+    temp = this.masterData.countries_stat.filter(a => a.total_recovered != "N/A");
+    temp1 = this.masterData.countries_stat.filter(a => a.total_recovered == "N/A");
+    this.masterData.countries_stat = [...temp,...temp1];
+
+    this.masterData.countries_stat.sort(function (a, b) {
+        return parseInt((b.active_cal).replace(/\,/g,'')) - parseInt((a.active_cal).replace(/\,/g,''));
+  })
+}
+  else if(event == "alphabetical"){
+    this.masterData.countries_stat.sort(function(a, b){
+    var nameA=a.country_name.toLowerCase(), nameB=b.country_name.toLowerCase()
+    if (nameA < nameB) //sort string ascendin
+          return -1 
+          if (nameA > nameB)
+              return 1
+          return 0 //default return value (no sorting)
+      })
+    }else if(event == "nofilter"){
+      this.masterData.countries_stat = _.orderBy(this.masterData.countries_stat,
+      [obj => parseFloat(obj.cases.replace(/,/g, ''))], ['desc']);
+    }
+
+
+  }
+
 
   getCountry(country) {
     if (country.length > 0) {
